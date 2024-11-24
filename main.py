69e40a038a83a51ecf28884d144c1d7a3cd42ca7
mewm0 <crcrvrv@protonmail.com>
2024-11-24 14:18:21 -0600

    added word counter to main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    with open('books/frankenstein.txt') as f:
+        file_contents = f.read()
+        words: list = file_contents.split()
+        print(len(words))
+
+if __name__ == "__main__":
+    main()
