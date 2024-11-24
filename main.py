378803ec58489b292d70ce29d16b80fb1e9fa14c
mewm0 <crcrvrv@protonmail.com>
2024-11-24 14:32:52 -0600

    added character counter

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 def main():
-    with open('books/frankenstein.txt') as f:
+    book_path = "books/frankenstein.txt"
+    with open(book_path) as f:
         file_contents = f.read()
-        words: list = file_contents.split()
-        print(len(words))
+        print(count_chars(file_contents))
+               
+
+def count_chars(text):
+    chars = {}
+    for c in text.lower():
+        if c not in chars:
+            chars[c] = 1
+        else:
+            chars[c] += 1
+    return chars
+
+def count_words(text):
+    words: list = text.split()
+    return len(words)
 
 if __name__ == "__main__":
     main()
