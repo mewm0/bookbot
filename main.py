b4b835109ce609b2d5ceecd37d3fdb940d19b583
mewm0 <crcrvrv@protonmail.com>
2024-11-24 14:50:03 -0600

    added reporting

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ def main():
     book_path = "books/frankenstein.txt"
     with open(book_path) as f:
         file_contents = f.read()
-        print(count_chars(file_contents))
-               
+        report(book_path, count_words(file_contents), count_chars(file_contents))
+
+
+def report(book_path, count_words, count_chars):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{count_words} words found in the document")
+    print("\n")
+    for char in count_chars:
+        print(f"The {char} character was found {count_chars[char]} times")
+    return
+
 
 def count_chars(text):
     chars = {}
